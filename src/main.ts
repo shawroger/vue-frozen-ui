8e939b189ec84ac1b7c85960774741ac5f743e29
shawroger <roger2000s@outlook.com>
2024-06-29 17:02:42 +0800

    use serve

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from "./App.vue";
 import "./components/frozen-ui.css";
 
 import FrozenUI from "./components";
-
-if (import.meta.env.DEV) {
-	const { createApp } = await import("vue");
-	createApp(App).use(FrozenUI).mount("#app");
-}
+const { createApp } = await import("vue");
+createApp(App).use(FrozenUI).mount("#app");
+// if (import.meta.env.DEV) {
+// 	const { createApp } = await import("vue");
+// 	createApp(App).use(FrozenUI).mount("#app");
+// }
