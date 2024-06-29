288e1e0740b8a209d5666fe1b30e48a9a0eb1c51
shawroger <roger2000s@outlook.com>
2024-06-29 17:07:54 +0800

    reset

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import App from "./App.vue";
 import "./components/frozen-ui.css";
 
 import FrozenUI from "./components";
-const { createApp } = await import("vue");
+
+import {createApp} from "vue"
 createApp(App).use(FrozenUI).mount("#app");
-// if (import.meta.env.DEV) {
-// 	const { createApp } = await import("vue");
-// 	createApp(App).use(FrozenUI).mount("#app");
-// }
+
